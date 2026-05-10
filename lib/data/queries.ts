77a33df57b0f4309851c5ce0e9ddb8946615289a
ertugrulerden <ertugrulerden05@gmail.com>
2026-05-10 20:28:59 +0300

    added sub-folders of admin dashboard

--- lib/data/queries.ts
+++ lib/data/queries.ts
@@ -1,5 +1,5 @@
 import db from "./db";
-import type { Restaurant, Platform } from "./types";
+import type { Restaurant, Platform, Product, Price, Detail } from "./types";
 
 export function getAllRestaurants(): Restaurant[]{
     const getAllRest = db.prepare("SELECT * FROM restaurants")
@@ -11,6 +11,18 @@ export function getAllPlatforms(): Platform[] {
     const getAllPlat = db.prepare("SELECT * FROM platforms")
     const platforms = getAllPlat.all() as Platform[]
     return platforms
+}
+
+export function getAllProducts(): Product[] {
+    return db.prepare("SELECT * FROM products ORDER BY name").all() as Product[]
+}
+
+export function getAllPrices(): Price[] {
+    return db.prepare("SELECT * FROM prices ORDER BY productID").all() as Price[]
+}
+
+export function getAllDetails(): Detail[] {
+    return db.prepare("SELECT * FROM details ORDER BY restaurantID").all() as Detail[]
 }
 
 
