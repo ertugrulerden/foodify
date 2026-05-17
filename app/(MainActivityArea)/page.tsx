e07b52aabce5a6a332ab32842ff35043e3d17f60
ertugrulerden <96317353+ertugrulerden@users.noreply.github.com>
2026-05-17 20:41:31 +0300

    hero + cards

--- app/(MainActivityArea)/page.tsx
+++ app/(MainActivityArea)/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from "@/components/Homepage/HeroSection"
+import RestaurantCard from "@/components/Homepage/RestaurantCard"
+import { popularRestaurants } from "@/lib/data/homepage"
 
 const HomePage = () => {
   return (
     <main>
       <HeroSection />
+      {popularRestaurants.map((r,i) => (
+			  <RestaurantCard key={i} {...r}/>
+		  ))}
     </main>
   )
 }
