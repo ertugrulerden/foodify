71230a868a01d14d19c765b4a4198657ecdfc8bf
ertugrulerden <96317353+ertugrulerden@users.noreply.github.com>
2026-05-17 21:31:50 +0300

    add search func to navbar search

--- components/Navbar/SearchBar.tsx
+++ components/Navbar/SearchBar.tsx
@@ -4,15 +4,14 @@ import { Input } from "../ui/input"
 
 const SearchBar = () => {
   return (
-
-    <div id="search" className="flex w-full max-w-sm items-center">
-        <Search className="relative left-7 h-6 w-6 text-muted-foreground" />
-        <Input
-            type="text" placeholder="Search menu or restaurant"
-            className="pl-10 w-full rounded-full bg-slate-100 border-transparent focus-visible:ring-1 focus-visible:bg-white"
-        />
-    </div>
-
+    <form action="/search" method="GET" className="relative flex w-full max-w-sm items-center">
+      <Search className="absolute left-3 h-5 w-5 text-muted-foreground pointer-events-none" />
+      <Input
+        type="text" placeholder="Search menu or restaurant"
+        name="q"
+        className="pl-10 w-fulsl rounded-full bg-slate-100 border-transparent focus-visible:ring-1 focus-visible:bg-white"
+      />
+    </form>
   )
 }
 
